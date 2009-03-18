e48657b5a1f38b5283ecfa0d96962029eb0f4100
kare1234 <kare1234@fe2871bc-0dc2-11de-a6e9-d320b29efae0>
2009-03-18 20:21:40 +0000

    Added support for the xml-file. - This means lots of new presets - We have to somehow give credit to the project who created the xml file? All preset are now in the presets menu Moved help text to the help menu. Removed the combobox

--- trunk/xmlparser/sinthxml.py
+++ trunk/xmlparser/sinthxml.py
@@ -10,28 +10,41 @@ def parseXML():
         xml_file = os.path.dirname(xml_file)
         xml_file = os.path.join(xml_file, "presets.xml")
 	optionsXML = etree.parse(xml_file)
-
+        
 	presets=[]
-	row = []
+	row = [' ',' ',' ',' ']
 
 	# Iterate through presets
 	for child in optionsXML.getiterator():
 		if child.tag == 'label':
-			row.append(child.text)
+			row[1]=child.text
 		if child.tag == 'params':
-			row.append(child.text)
+			row[2]=child.text
 		if child.tag == 'extension':
-			row.append(child.text)
+			row[3]=child.text
 		if child.tag == 'category':
-			row.append(child.text)
+   			row[0]=child.text
+			row[0]=child.text
 			presets.append(row)
-			row = []
-	# Sort by category
-	presets.sort(lambda x, y: cmp(x[3],y[3]))
+			row = [' ',' ',' ',' ']
+    	# Sort by category
+	presets.sort(lambda x, y: cmp(x[0],y[0]))
+    	# find category list
+    	categorylist=[presets[0][0]]
+    	for row in presets:
+        	if row[0]!=categorylist[-1]:
+            		categorylist.append(row[0])
+        
+    	print categorylist
+# print for debugging
 	for row in presets:
 		print row	
-	#print presets
+	print presets
+    
 
+
+	presetlist=presets
+    
 	
 	# Print some stuff
 #	print presets[0][0]
