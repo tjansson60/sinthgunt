1c77039f43756ca7900352d8fb2eabe98ecf5732
Thomas Jansson <tjansson60@gmail.com>
2009-07-24 09:13:30 +0000

    Updated tarmaker.sh script to include icon.png

--- tarmaker.sh
+++ tarmaker.sh
@@ -1,4 +1,9 @@
-tar -cvvf sinthgunt-latest.tar sinthgunt.py sinthgunt.glade presets.xml logo.png README.txt LICENSE.txt
-gzip sinthgunt-latest.tar
+#!/bin/bash
 
-pydoc -w sinthgunt
+#This line collects and compresses the files for the tarball
+tar -cvzf sinthgunt-latest.tar.gz sinthgunt.py sinthgunt.glade presets.xml \
+	      icon.png logo.png README.txt LICENSE.txt setup.py
+
+# Creates html documentation. Why is this a part of the tarmake
+# script? It is not even included in the tarball.
+#pydoc -w sinthgunt
