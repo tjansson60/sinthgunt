7d5cce36e0881ebc3301d1032ffd1d4373a75661
l.keijser <l.keijser@fe2871bc-0dc2-11de-a6e9-d320b29efae0>
2009-08-09 17:53:00 +0000

    Created directory structure for new setup.py. Replaced setup.py with newer version.

--- branches/sinthgunt-leon/setup.py
+++ branches/sinthgunt-leon/setup.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os, sys
 
-setup(name='Sinthgunt',
-        version='1.0',
-        description='Sinthgunt Converter',
-        author='Kaare Hartvig Jensen and Thomas R. N. Janson',
-        url='http://sinthgunt.googlecode.com',
-        scripts = ['sinthgunt.py'],
-        data_files=[('share/sinthgunt',['sinthgunt.glade','presets.xml','logo.png','icon.png','README.txt','LICENSE.txt'])]
+files=[]
+for f in os.path.abspath(''):
+    files.append(f)
+
+setup(name = 'sinthgunt',
+    version = '1.0',
+    description = 'Sinthgunt Converter',
+    long_description = 'Sinthgunt Converter',
+    author = 'Kaare Hartvig Jensen and Thomas R. N. Janson',
+    author_email = 'kare1234@gmail.com',
+    url = 'http://sinthgunt.googlecode.com',
+    license = ['GPLv3'],
+    packages = ['Sinthgunt'],
+    package_data = {'sinthgunt': files},
+    scripts = ['sinthgunt'],
+    data_files=[
+        ('share/sinthgunt',['share/sinthgunt.glade','share/presets.xml','share/logo.png','share/icon.png','README.txt','LICENSE.txt'])
+    ]
 )
 
--- branches/sinthgunt-leon/tarmaker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#This line collects and compresses the files for the tarball
-tar -cvzf sinthgunt-latest.tar.gz sinthgunt.py sinthgunt.glade presets.xml \
-	      icon.png logo.png README.txt LICENSE.txt setup.py
-
-# Creates html documentation. Why is this a part of the tarmake
-# script? It is not even included in the tarball.
-#pydoc -w sinthgunt
